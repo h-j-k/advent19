9048f2bfa11b2e74bfb07ef6f3768d2022cf8f37
HJK <han.junkwang@gmail.com>
2021-11-16 18:51:16 +0800

    Day 1 and 2 refactoring.

--- src/Day01.hs
+++ src/Day01.hs
@@ -4,18 +4,18 @@ module Day01
   )
 where
 
-part1 :: [Int] -> Int
-part1 inputs = sum (map fuelForModule inputs)
-
 fuelForModule :: Int -> Int
-fuelForModule x 
- | x >= 6 = div x 3 - 2
- | otherwise = 0 
-
-part2 :: [Int] -> Int
-part2 inputs = sum (map fuelForModule2 inputs)
+fuelForModule x
+  | x >= 6 = div x 3 - 2
+  | otherwise = 0
 
 fuelForModule2 :: Int -> Int
 fuelForModule2 x
   | x <= 0 = 0
-  | otherwise = fuelForModule x + fuelForModule2(fuelForModule x)
+  | otherwise = fuelForModule x + fuelForModule2 (fuelForModule x)
+
+part1 :: [Int] -> Int
+part1 = sum . fmap fuelForModule
+
+part2 :: [Int] -> Int
+part2 = sum . fmap fuelForModule2
--- src/Day02.hs
+++ src/Day02.hs
@@ -6,27 +6,6 @@ where
 
 import qualified Data.Array as Array
 
-part1 :: [Int] -> Int
-part1 inputs =
-  case intCodeMod (Array.listArray (0, length inputs - 1) inputs) 1202 of
-    Just answer -> answer
-    Nothing -> error "Error!"
-
-part2 :: [Int] -> Int -> Int
-part2 inputs = part2do (Array.listArray (0, length inputs - 1) inputs) 0
-
-part2do :: Array.Array Int Int -> Int -> Int -> Int
-part2do array n target =
-  case intCodeMod array n of
-    Just answer -> if answer == target then n else part2do array (n + 1) target
-    Nothing -> part2do array (n + 1) target
-
-intCodeMod :: Array.Array Int Int -> Int -> Maybe Int
-intCodeMod array n =
-  case intCode (array Array.// [(1, div n 100), (2, mod n 100)]) 0 of
-    Just instructions -> Just (instructions Array.! 0)
-    Nothing -> Nothing
-
 intCode :: Array.Array Int Int -> Int -> Maybe (Array.Array Int Int)
 intCode instructions p =
   let opCode = instructions Array.! p
@@ -45,3 +24,24 @@ intCode instructions p =
                     2 -> intCode (instructions Array.// [(c, x * y)]) (p + 4)
                     _ -> Nothing
             else Nothing
+
+part2do :: Array.Array Int Int -> Int -> Int -> Int
+part2do array n target =
+  case intCodeMod array n of
+    Just answer -> if answer == target then n else part2do array (n + 1) target
+    Nothing -> part2do array (n + 1) target
+
+intCodeMod :: Array.Array Int Int -> Int -> Maybe Int
+intCodeMod array n =
+  case intCode (array Array.// [(1, div n 100), (2, mod n 100)]) 0 of
+    Just instructions -> Just (instructions Array.! 0)
+    Nothing -> Nothing
+
+part1 :: [Int] -> Int
+part1 inputs =
+  case intCodeMod (Array.listArray (0, length inputs - 1) inputs) 1202 of
+    Just answer -> answer
+    Nothing -> error "Error!"
+
+part2 :: [Int] -> Int -> Int
+part2 inputs = part2do (Array.listArray (0, length inputs - 1) inputs) 0
